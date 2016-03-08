45f802a48aba81c6098bf4b00fbe0485a3b59949
ArtWhite <sjobs3801@gmail.com>
2016-03-08 18:43:28 +0300

    Test

---
+++ src/ru/ArtWhite/Politota/CanvasView.java
@@ -0,0 +1,23 @@
+package ru.ArtWhite.Politota;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+
+/*
+ * Created by ArtWhite on 08.03.16.
+ */
+public class CanvasView extends View {
+
+    public CanvasView(Context context, AttributeSet attrs){
+
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
---
+++ src/ru/ArtWhite/Politota/MainCircle.java
@@ -0,0 +1,6 @@
+package ru.ArtWhite.Politota;
+
+/**
+ * Created by ArtWhite on 08.03.16.
+ */
+public class MainCircle {
